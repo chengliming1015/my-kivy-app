7dda9c6ebf16148e2e2d9d58462fbf908e635d06
chengliming1015 <1962235137@qq.com>
2026-01-02 18:30:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
 
-class MyKivyApp(App):
+class MainApp(App):
     def build(self):
-        layout = BoxLayout(orientation='vertical', spacing=20, padding=50)
-        title_label = Label(
-            text="Kivy GitHub Actions 打包示例",
-            font_size=30,
-            bold=True
-        )
-        desc_label = Label(
-            text="APK 构建成功！\n可直接复用此模板开发上层功能",
-            font_size=20
-        )
-        layout.add_widget(title_label)
-        layout.add_widget(desc_label)
-        return layout
+        return Label(text='Hello Kivy! 官方零错打包', font_size=50)
 
 if __name__ == '__main__':
-    MyKivyApp().run()
+    MainApp().run()
